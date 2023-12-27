cefeb64326e3f86e2d5f96af9ea9cf4d3e96d389
João Pedro Macedo Oliveira <joao.pmoliveira@outlook.com>
2023-12-26 22:28:50 -0300

    uv script

---
+++ scripts/Brush.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+@export var texture: Texture2D
+@export var brush_size: int = 100
+
+var brush_queue = []
+
+func _draw():
+	pass
---
+++ scripts/SetUVPositionMesh.gd
@@ -0,0 +1,4 @@
+extends MeshInstance3D
+
+func _ready():
+	LevelUVPosition.set_mesh(self)
